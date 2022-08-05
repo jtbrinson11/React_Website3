4fd8baef8040f44f6967dc428cbdc2fd6e160bb3
Joel Brinson <jtbrinson11@gmail.com>
2022-08-05 15:17:16 -0400

    tweaked project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Pics from './Pics';
+import AboutUs from './AboutUs';
+import ContactUs from './ContactUs';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}></Route>
+        <Route path='/pics' element={<Pics />}></Route>
+        <Route path='/about' element={<AboutUs />}></Route>
+        <Route path='/contact' element={<ContactUs />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
